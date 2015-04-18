af838f8c6c6b3e4e4ee5f3c180df290ed2447d0a
alevere <alevere@fishs-MacBook-Pro.local>
2015-04-18 19:13:14 -0400

    altering cookie path

--- incognito.js
+++ incognito.js
@@ -8,11 +8,23 @@ function listener(incomingHeaders) {
         var requestURL = incomingHeaders.url;
         var requestTab = incomingHeaders.tabId;
         var Headers = incomingHeaders.responseHeaders;
+        var valueArray;
+        var temp;
+        var path_found = false;
         for (index=0; index<Headers.length; index++) {
             if (Headers[index].name.toUpperCase()===setcookie) {
               console.log(Headers[index]);
-              Headers[index].value="testing";
+              valueArray = Headers[index].value.split(";");
+                for (indexA=0; indexA<valueArray.length; indexA++){
+                    temp = valueArray[indexA].toUpperCase();
+                    temp = temp.trim();
+                    temp = temp.substring(0,4);
+                    if (temp === "PATH=") {path_found=true;valueArray[indexA].replace("=","=/"+requestTab)}
+                }
+                if (!path_found) {valueArray[indexA]="path=/"+requestTab;}
+              Headers[index].value=Headers[index].value.replace("path=/", "path=/1/");
               console.log(Headers[index]);
+              console.log("true");
             }
         }
     }
