082dada6c5508040772c9f03ae76f118669d8a66
alevere <alevere@fishs-MacBook-Pro.local>
2015-04-18 17:59:18 -0400

    intercepting set-cookie

--- incognito.js
+++ incognito.js
@@ -1,10 +1,20 @@
-//listen for changes to cookies
-chrome.cookies.onChanged.addListener(listener);
-
-//function to display cookie info
-function listener(info) {
-    if (info!=null){
-       var cookie = info.cookie;
-        console.log("dom:" + cookie.domain + ", jar:" + cookie.storeId + "\r\n");
+//listen for network responses
+chrome.webRequest.onHeadersReceived.addListener(listener,{ urls: ["<all_urls>"]}, ["blocking", "responseHeaders"]);
+var secretKey = 45;
+var setcookie = "SET-COOKIE";
+//function to blah
+function listener(incomingHeaders) {
+    if (incomingHeaders!=null){
+        var requestURL = incomingHeaders.url;
+        var requestTab = incomingHeaders.tabId;
+        var Headers = incomingHeaders.responseHeaders;
+        for (index=0; index<Headers.length; index++) {
+            if (Headers[index].name.toUpperCase()===setcookie) {
+              console.log(Headers[index]);
+              Headers[index].value="testing";
+              console.log(Headers[index]);
+            }
+        }
     }
+    return {responseHeaders: Headers};
 }
